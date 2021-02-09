dd21228cffb21b2caf96a97bd40d0f67b17d0b69
Felipe Rezende <felipe.rezende149i@gmail.com>
2021-02-09 14:20:08 -0300

    Add files via upload

---
+++ Timers/timers_Felipe_Rezende/Debug/app/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../app/app.c \
+../app/hw.c 
+
+OBJS += \
+./app/app.o \
+./app/hw.o 
+
+C_DEPS += \
+./app/app.d \
+./app/hw.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+app/app.o: ../app/app.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F103xB -DDEBUG -c -I../Core/Inc -I../app -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"app/app.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+app/hw.o: ../app/hw.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F103xB -DDEBUG -c -I../Core/Inc -I../app -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"app/hw.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
